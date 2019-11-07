4658fd30d35987569697c730b4297649b46b3bbb
Okatyev Alexander <a.okatyev@bssys.com>
2019-11-07 18:07:28 +0400

    теория - работа с формой

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,38 +1,95 @@
 <template>
   <div id="app">
-      <h2>{{ title }}</h2>
-      <h2>{{ title | lowercase }}</h2>
+    <input type="text" v-model.lazy="name">
+    <input type="text" v-model.number="age">
+    <p>{{ name }}</p>
+    <textarea v-model="textarea">
 
-    <input type="text" v-model="searchName">
+    </textarea>
+    <p>{{ textarea }}</p>
+
+    <hr>
+
+    <label>
+      <input type="checkbox" value="instagram" v-model="social"> instagram
+    </label>
+
+    <label>
+      <input type="checkbox" value="vk" v-model="social"> vk
+    </label>
+
+    <label>
+      <input type="checkbox" value="facebook" v-model="social"> facebook
+    </label>
 
     <ul>
-      <li v-for="name of filteredNames">{{ name }}</li>
+      <li v-for="s in social">{{ s }}</li>
     </ul>
 
-    <app-list></app-list>
+    <hr>
+    <h2>radio buttons</h2>
+
+    <label>
+      <input type="radio" value="instagram" v-model="rSocial"> instagram
+    </label>
+
+    <label>
+      <input type="radio" value="vk" v-model="rSocial"> vk
+    </label>
+
+    <label>
+      <input type="radio" value="facebook" v-model="rSocial"> facebook
+    </label>
+
+    <ul>
+      <li>{{ rSocial }}</li>
+    </ul>
+
+    <hr>
+
+    <h2>select</h2>
+    <select v-model="defaultSelect">
+      <option v-for="o in selectList" >{{o}}</option>
+    </select>
   </div>
 </template>
 
 <script>
 
-  import ListMixin from './listMixin';
 
 export default {
   data () {
     return {
-      title: 'Hello',
-      names: ['Sergey', 'Alena', 'Ivan']
+      name: '',
+      textarea: 'initial text',
+      social: [],
+      rSocial: '',
+      selectList: ['instagram', 'vk', 'facebook'],
+      defaultSelect: '',
+      age: 20
     }
   },
-  mixins: [ListMixin],
-  filters: {
-    lowercase(value) {
-      return value.toLowerCase();
+  watch: {
+    age (value) {
+      console.log(value);
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    margin-bottom: 10px;
+  }
+  textarea {
+    resize: none;
+    width: 100%;
+    min-height: 100px;
+  }
+  p {
+    white-space: pre;
+  }
 </style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,15 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import List from './List'
-Vue.filter('uppercase', value => value.toUpperCase())
-
-Vue.component('app-list', List);
-
-Vue.mixin({
-  beforeCreate() {
-    console.log('before create');
-  }
-})
 
 new Vue({
   el: '#app',
